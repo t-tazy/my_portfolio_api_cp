0bc67675afe17fe35244da7c981ef9969818c313
t-tazy <takumin390511@gmail.com>
2022-12-11 19:16:17 +0900

    feat(main.go): 最小構成のサーバーを実装

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/t-tazy/my_portfolio_api
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+// :18080でリッスンし、リクエストパスをレスポンスとして返す。
+// エラーが発生した場合、終了コード1でプログラムを終了する。
+func main() {
+	err := http.ListenAndServe(
+		":18080",
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	)
+	if err != nil {
+		fmt.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
+	}
+}
