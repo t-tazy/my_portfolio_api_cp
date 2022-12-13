3376ca51a29790ef9505cf12025f6d1e1057960d
t-tazy <takumin390511@gmail.com>
2022-12-14 08:41:54 +0900

    feat(main.go): メインプロセスをrun関数へ移譲

--- main.go
+++ main.go
@@ -1,22 +1,44 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
 )
 
-// :18080でリッスンし、リクエストパスをレスポンスとして返す。
-// エラーが発生した場合、終了コード1でプログラムを終了する。
+// メインプロセスのrun関数を実行する
 func main() {
-	err := http.ListenAndServe(
-		":18080",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server: %v", err)
+	}
+}
+
+// :18080でリッスンし、リクエストパスをレスポンスとして返すHTTPサーバーを起動
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
-	)
-	if err != nil {
-		fmt.Printf("failed to terminate server: %v", err)
-		os.Exit(1)
 	}
+	errCh := make(chan error)
+
+	// 別ゴルーチンでHTTPサーバーを起動
+	go func() {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			errCh <- err
+		}
+		errCh <- nil
+	}()
+
+	// コンテキストを通じて処理の中断を検知したとき
+	// ShutdownメソッドでHTTPサーバーの機能を終了する
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+
+	return <-errCh
 }
