f60b9f6e0a4349bdd552a04fe8b0eaa9a6d67f9f
t-tazy <takumin390511@gmail.com>
2022-12-15 17:24:25 +0900

    fix(main.go): run関数のシグネチャを変更したが、呼び出し時の引数が未変更のままだったため修正

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 
 // メインプロセスのrun関数を実行する
 func main() {
-	if err := run(context.Background(), l); err != nil {
+	if err := run(context.Background()); err != nil {
 		log.Printf("failed to terminate server: %v", err)
 		os.Exit(1)
 	}
@@ -27,7 +27,7 @@ func run(ctx context.Context) error {
 	}
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
 	if err != nil {
-		log.Fatalf("failed to listen port %s: %v", cfg.Port, err)
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
 	}
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
