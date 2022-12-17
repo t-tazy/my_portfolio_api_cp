e8a2173397c38850e2136d46b243247135c39dc8
t-tazy <takumin390511@gmail.com>
2022-12-17 16:22:36 +0900

    feat: ヘルパー関数の作成
    handler/response.goにHTTPハンドラー用のヘルパーを作成
    testutil/handler.goにHTTPハンドラーのテスト用のヘルパーを作成

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/t-tazy/my_portfolio_api
 
 go 1.19
 
-require github.com/caarlos0/env/v6 v6.10.1
+require (
+	github.com/caarlos0/env/v6 v6.10.1
+	github.com/google/go-cmp v0.5.9
+)
---
+++ testutil/handler.go
@@ -0,0 +1,60 @@
+package testutil
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+// レスポンスと期待値を比較検証するテスト用ヘルパー
+func AssertJSON(t *testing.T, want, got []byte) {
+	t.Helper()
+
+	// 任意の構造のJSONをデコードするため、デコード先はany型とする
+	var jsonWant, jsonGot any
+	if err := json.Unmarshal(want, &jsonWant); err != nil {
+		t.Fatalf("cannot unmarshal want %q: %v", want, err)
+	}
+	if err := json.Unmarshal(got, &jsonGot); err != nil {
+		t.Fatalf("cannot unmarshal got %q: %v", got, err)
+	}
+	if diff := cmp.Diff(jsonGot, jsonWant); diff != "" {
+		t.Errorf("got differs: (-got +want)\n%s", diff)
+	}
+}
+
+func AssertResponse(t *testing.T, got *http.Response, status int, body []byte) {
+	t.Helper()
+
+	t.Cleanup(func() { _ = got.Body.Close() })
+
+	gb, err := io.ReadAll(got.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got.StatusCode != status {
+		t.Fatalf("want status %d, but got %d, body: %q", status, got.StatusCode, gb)
+	}
+	if len(gb) == 0 && len(body) == 0 {
+		// 期待としても実体としてもレスポンスボディがないため
+		// AssertJSONを呼ぶ必要がない
+		return
+	}
+	AssertJSON(t, body, gb)
+}
+
+// ゴールデンテスト用
+// 入力値・期待値をファイルから取得するテスト用ヘルパー
+func LoadFile(t *testing.T, path string) []byte {
+	t.Helper()
+
+	bt, err := os.ReadFile(path)
+	if err != nil {
+		t.Fatalf("cannot read from %q: %v", path, err)
+	}
+	return bt
+}
