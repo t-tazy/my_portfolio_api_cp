6fbd6e73b9134ee7e3912e89ced5db6345eb1265
t-tazy <takumin390511@gmail.com>
2022-12-29 07:02:07 +0900

    feat: User型の定義
    re #7

--- _tools/mysql/schema.sql
+++ _tools/mysql/schema.sql
@@ -3,6 +3,7 @@ CREATE TABLE users
   id       BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ユーザーの識別子',
   name     VARCHAR(255) NOT NULL COMMENT 'ユーザー名',
   password VARCHAR(80) NOT NULL COMMENT 'パスワードハッシュ',
+  role     VARCHAR(80) NOT NULL COMMENT 'ロール',
   created  DATETIME(6) NOT NULL COMMENT 'レコード作成日時',
   modified DATETIME(6) NOT NULL COMMENT 'レコード修正日時',
   PRIMARY KEY (id),
