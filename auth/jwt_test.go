3054f94f41580b191ae3cc6a804e7bb6d01130f5
t-tazy <takumin390511@gmail.com>
2023-01-01 05:06:39 +0900

    feat, test: 鍵ファイルを埋め込む
    re #9

---
+++ auth/jwt_test.go
@@ -0,0 +1,19 @@
+package auth
+
+import (
+	"bytes"
+	"testing"
+)
+
+// 鍵ファイルが埋め込めているかテスト
+func TestEmbed(t *testing.T) {
+	want := []byte("-----BEGIN PUBLIC KEY-----")
+	if !bytes.Contains(rawPubKey, want) {
+		t.Errorf("want %s, but got %s", want, rawPubKey)
+	}
+
+	want = []byte("-----BEGIN PRIVATE KEY-----")
+	if !bytes.Contains(rawPrivKey, want) {
+		t.Errorf("want %s, but got %s", want, rawPrivKey)
+	}
+}
