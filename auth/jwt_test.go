4213703e2e300ea1cfdb8faa01aa0789f001d1cf
t-tazy <takumin390511@gmail.com>
2023-01-02 07:14:38 +0900

    test: JWT発行処理をテスト
    ダミーユーザーの作成にfixtureパッケージを用意
    
    re #9

--- auth/jwt_test.go
+++ auth/jwt_test.go
@@ -2,7 +2,12 @@ package auth
 
 import (
 	"bytes"
+	"context"
 	"testing"
+
+	"github.com/t-tazy/my_portfolio_api/clock"
+	"github.com/t-tazy/my_portfolio_api/entity"
+	"github.com/t-tazy/my_portfolio_api/testutil/fixture"
 )
 
 // 鍵ファイルが埋め込めているかテスト
@@ -15,5 +20,29 @@ func TestEmbed(t *testing.T) {
 	want = []byte("-----BEGIN PRIVATE KEY-----")
 	if !bytes.Contains(rawPrivKey, want) {
 		t.Errorf("want %s, but got %s", want, rawPrivKey)
+	}
+}
+
+func TestJWTer_GenerateToken(t *testing.T) {
+	ctx := context.Background()
+	moq := &StoreMock{}
+	wantID := entity.UserID(10)
+	u := fixture.User(&entity.User{ID: wantID}) // ダミー生成
+	moq.SaveFunc = func(ctx context.Context, key string, userID entity.UserID) error {
+		if userID != wantID {
+			t.Errorf("want %d, but got %d", wantID, userID)
+		}
+		return nil
+	}
+	sut, err := NewJWTer(moq, clock.FixedClocker{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := sut.GenerateToken(ctx, u)
+	if err != nil {
+		t.Fatalf("not want err: %v", err)
+	}
+	if len(got) == 0 {
+		t.Errorf("token is empty")
 	}
 }
