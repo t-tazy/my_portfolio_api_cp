f50295776d5b0d23d69cf331598b59cd8fca49bf
t-tazy <takumin390511@gmail.com>
2023-01-03 07:23:03 +0900

    feat: HTTPリクエストからJWTを取得
    リクエストヘッダーからJWTを取得し、それから取得したリクエストスコープな値をcontextに含めるように実装。
    
    re #9

--- auth/jwt.go
+++ auth/jwt.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	_ "embed"
 	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/google/uuid"
@@ -39,6 +40,7 @@ type Store interface {
 	Load(ctx context.Context, key string) (entity.UserID, error)
 }
 
+// 鍵として読み込んだデータをJWTer型として保持
 func NewJWTer(s Store, c clock.Clocker) (*JWTer, error) {
 	j := &JWTer{Store: s}
 	privkey, err := parse(rawPrivKey)
@@ -89,4 +91,78 @@ func (j *JWTer) GenerateToken(ctx context.Context, u *entity.User) ([]byte, erro
 		return nil, err
 	}
 	return signed, nil
+}
+
+// HTTPリクエストからJWTを取得する
+func (j *JWTer) GetToken(ctx context.Context, r *http.Request) (jwt.Token, error) {
+	token, err := jwt.ParseRequest(
+		r,
+		jwt.WithKey(jwa.RS256, j.PublicKey),
+		jwt.WithValidate(false),
+	)
+	if err != nil {
+		return nil, err
+	}
+	if err := jwt.Validate(token, jwt.WithClock(j.Clocker)); err != nil {
+		return nil, fmt.Errorf("GetToken: failed to validate token: %w", err)
+	}
+	// 期限切れではないことを確認
+	if _, err := j.Store.Load(ctx, token.JwtID()); err != nil {
+		return nil, fmt.Errorf("GetToken: %q expired: %w", token.JwtID(), err)
+	}
+	return token, nil
+}
+
+type userIDKey struct{} // contextのkeyとして使う独自型
+type roleKey struct{}   // contextのkeyとして使う独自型
+
+// contextにユーザーIDを付加
+func SetUserID(ctx context.Context, uid entity.UserID) context.Context {
+	return context.WithValue(ctx, userIDKey{}, uid)
+}
+
+// contextからユーザーIDを取得
+func GetUserID(ctx context.Context) (entity.UserID, bool) {
+	id, ok := ctx.Value(userIDKey{}).(entity.UserID)
+	return id, ok
+}
+
+// tokenからロール情報を取り出し、contextに付加
+func SetRole(ctx context.Context, token jwt.Token) context.Context {
+	get, ok := token.Get(RoleKey)
+	if !ok {
+		return context.WithValue(ctx, roleKey{}, "")
+	}
+	return context.WithValue(ctx, roleKey{}, get)
+}
+
+// contextからユーザーロールを取得
+func GetRole(ctx context.Context) (string, bool) {
+	role, ok := ctx.Value(roleKey{}).(string)
+	return role, ok
+}
+
+// リクエストスコープな値をcontextに含める
+func (j *JWTer) FillContext(r *http.Request) (*http.Request, error) {
+	token, err := j.GetToken(r.Context(), r)
+	if err != nil {
+		return nil, err
+	}
+	uid, err := j.Store.Load(r.Context(), token.JwtID())
+	if err != nil {
+		return nil, err
+	}
+	ctx := SetUserID(r.Context(), uid)
+	ctx = SetRole(ctx, token)
+	clone := r.Clone(ctx)
+	return clone, nil
+}
+
+// contextから管理者権限の有無を確認する
+func IsAdmin(ctx context.Context) bool {
+	role, ok := GetRole(ctx)
+	if !ok {
+		return false
+	}
+	return role == "admin"
 }
